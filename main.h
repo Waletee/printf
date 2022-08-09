0cfb7181b7288dc12269986772b6768b8b5d8a8c
waletee <taiwobiodun@gmail.com>
2022-08-09 23:18:47 +0100

    editing and updating the main.h file

--- main.h
+++ main.h
@@ -10,5 +10,23 @@ int print_rev(va_list r);
 int print_rot13(va_list R);
 int print_char(va_list c);
 int print_porcentage(void);
+int print_bin(va_list b);
+int print_unsig(va_list u);
+int print_octal(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+
+/**
+  * struct code_format - Struct format
+  *
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+ 
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
 
 #endif
